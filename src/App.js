1687efea17074ca6aff08bcd4eaf1b366ec342c6
iphadan <ephadaniel177@gmail.com>
2024-09-20 21:04:18 +0300

    history added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,53 @@ function App() {
   return <Board/>
 }
 
-export default App;
+
+export default function Game() {
+  const [history,setHistory] = useState([Array(9).fill(null)])
+
+  
+  const [currentMove,setCurrentMove] = useState(0)
+  const currentSquares = history[currentMove];
+  let xIsNext = currentMove % 2 === 0;
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+
+  }
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  
+  }
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove); 
+
+  }
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move} >
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol >{moves}</ol>
+      </div>
+    </div>
+  );
+
+}
 function calculateWinner(squares) {
 
   const lines = [
@@ -23,16 +69,14 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      console.log("winnder")
+      console.log("winner")
       return squares[a];
     }
   }
   return null;
 }
 
-export function Board() {
-  const [squares,setSquares] = useState(Array(9).fill(null));
-  const [xIsNext,setXIsNext] = useState(true);
+export function Board({xIsNext,squares,onPlay}) {
   const winner = calculateWinner(squares);
   let status;
   if(winner){
@@ -47,7 +91,6 @@ function handleClick(i){
   if(!squares[i] && !winner){
   if(xIsNext){
     nextSquares[i] = "X";
-    setXIsNext(false);
    
 
   }
@@ -55,10 +98,10 @@ function handleClick(i){
     
      nextSquares[i] = "O";
      
-    setXIsNext(true);
   }
+  onPlay(nextSquares)
   
-  setSquares(nextSquares);
+ 
   
 }
 
@@ -67,9 +110,7 @@ return
 }
 }
 
-function click(){
-  console.log("clicked");
-}
+
 
   return <>
  <div className="status">{status}</div>
