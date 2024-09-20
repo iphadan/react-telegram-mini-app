88a669467b87a8aace1bb3fdb685b7614bf393bd
iphadan <ephadaniel177@gmail.com>
2024-09-20 16:10:57 +0300

    Board and Square components added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return <button className='square'>X</button>
+  return <Board/>
 }
 
 export default App;
+
+
+export function Board() {
+
+  return <>
+  <div className='board-row'>
+<Square /> <Square /> <Square />
+  </div>
+   
+  <div className='board-row'>  
+  <Square/> <Square/> <Square/>
+  </div>
+
+  <div className='board-row'> 
+  <Square/> <Square/> <Square/>
+  </div>
+
+  </>
+}
+ function Square(){ 
+  const [value,setValue] = useState(null);
+
+  function handleClick(){
+    setValue("X");
+  }
+  return <button className='square' onClick={handleClick}>{value}</button>
+ }
